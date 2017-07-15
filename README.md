17970f1f5179dfe447ddebf277e531677532232f
mercy ngatia <mercyngatia02@mail.com>
2017-07-15 14:55:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+An Android app that uses TmdbAPI display a list of movies.
---
+++ app/src/main/java/com/example/student/movieapp/adapter/MoviesAdapter.java
@@ -0,0 +1,58 @@
+package com.example.student.movieapp.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.student.movieapp.R;
+import com.example.student.movieapp.models.Movie;
+
+import java.util.List;
+
+/**
+ * Created by USER on 7/15/2017.
+ */
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            genre = (TextView) view.findViewById(R.id.genre);
+            year = (TextView) view.findViewById(R.id.year);
+        }
+    }
+
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()) //fills the layout
+                .inflate(R.layout.movie_list_row, parent, false); //what layout are you inflating
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) { //gets whataever in the movielist and send it to the view
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+}
