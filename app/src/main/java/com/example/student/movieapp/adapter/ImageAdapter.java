aba094fcccfdfcfce66a9151ff6fc312ae2574b0
mercy ngatia <mercyngatia02@mail.com>
2017-10-07 09:33:30 +0300

    TMdb retrofit

---
+++ app/src/main/java/com/example/student/movieapp/adapter/ImageAdapter.java
@@ -0,0 +1,11 @@
+package com.example.student.movieapp.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by USER on 9/30/2017.
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter{
+
+}
